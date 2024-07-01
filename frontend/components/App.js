a0359e46627981f3b57735e6267426651ca2d16c
Chaunceyo <chaunceyogletree725@gmail.com>
2024-07-01 02:56:23 -0400

    almost done

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -5,11 +5,15 @@ import LoginForm from './LoginForm'
 import Message from './Message'
 import ArticleForm from './ArticleForm'
 import Spinner from './Spinner'
+import axios from 'axios'
 
 const articlesUrl = 'http://localhost:9000/api/articles'
 const loginUrl = 'http://localhost:9000/api/login'
 
 export default function App() {
+
+  
+
   // ✨ MVP can be achieved with these states
   const [message, setMessage] = useState('')
   const [articles, setArticles] = useState([])
@@ -18,8 +22,8 @@ export default function App() {
 
   // ✨ Research `useNavigate` in React Router v.6
   const navigate = useNavigate()
-  const redirectToLogin = () => { /* ✨ implement */ }
-  const redirectToArticles = () => { /* ✨ implement */ }
+  const redirectToLogin = () => { navigate("/")}
+  const redirectToArticles = () => {navigate("/articles") }
 
   const logout = () => {
     // ✨ implement
@@ -27,6 +31,9 @@ export default function App() {
     // and a message saying "Goodbye!" should be set in its proper state.
     // In any case, we should redirect the browser back to the login screen,
     // using the helper above.
+    localStorage.removeItem('token')
+    setMessage("Goodbye!")
+    redirectToLogin()
   }
 
   const login = ({ username, password }) => {
@@ -36,9 +43,40 @@ export default function App() {
     // On success, we should set the token to local storage in a 'token' key,
     // put the server success message in its proper state, and redirect
     // to the Articles screen. Don't forget to turn off the spinner!
+
+    //FETCH, NAVIGATE AND ADD TO PROPS
+    setMessage('')
+    setSpinnerOn(true)
+    fetch(loginUrl, {
+      method: "POST",
+      body: JSON.stringify({
+        username,
+        password
+      }),
+      headers: {
+        'Content-Type' : 'application/json'
+      }
+    })
+    .then(response => {
+      if (!response.ok) {
+        throw new Error(`Ouch, status ${response.status}`)
+      }
+      return response.json()
+    })
+    .then(data => {
+      localStorage.setItem('token', data.token)
+      setMessage(data.message)
+      redirectToArticles()
+      setSpinnerOn(false)
+    })
+    .catch(error => {
+      console.error('Error: ', error)
+      redirectToLogin()
+      setSpinnerOn(false)
+    }) 
   }
 
-  const getArticles = () => {
+  const getArticles = async ()  => {
     // ✨ implement
     // We should flush the message state, turn on the spinner
     // and launch an authenticated request to the proper endpoint.
@@ -47,29 +85,162 @@ export default function App() {
     // If something goes wrong, check the status of the response:
     // if it's a 401 the token might have gone bad, and we should redirect to login.
     // Don't forget to turn off the spinner!
+
+    //FETCH AND ADD TO PROPS
+    setMessage('')
+    setSpinnerOn(true)
+      try{
+        const {data} = await axios.get(
+          articlesUrl,
+          {headers : {Authorization : localStorage.getItem('token')}}
+        )
+          
+          console.log(data)
+          setArticles(data.articles)
+          setMessage(data.message)
+          setSpinnerOn(false)
+
+      }catch (error) {
+        if(error?.response?.status === 401) {redirectToLogin()
+        setSpinnerOn(false)
+        setMessage(error.response.data.message) 
+        }
+      }
+    
+    
   }
 
-  const postArticle = article => {
+  const postArticle = (article) => {
     // ✨ implement
     // The flow is very similar to the `getArticles` function.
     // You'll know what to do! Use log statements or breakpoints
     // to inspect the response from the server.
+
+    //FETCH AND ADD TO PROPS
+    setMessage('')
+    setSpinnerOn(true)
+    fetch(articlesUrl, {
+      method: 'POST',
+      body: JSON.stringify({
+        title: article.title,
+        text: article.text,
+        topic: article.topic
+      }),
+      headers: new Headers({
+        'Content-Type' : 'application/json',
+        Authorization : localStorage.getItem('token')
+      })
+    })
+    .then(res => {
+      if(!res.ok){
+        throw new Error("Can't POST article", res.status)
+      }
+      return res.json()
+    })
+    .then(data => {
+      console.log("POST articles data: ", data)
+      setMessage(data.message)
+      setSpinnerOn(false)
+    })
+    .catch(error => {
+      if(error?.response?.status === 401) redirectToLogin()
+      setSpinnerOn(false)
+    })
+    
+    
   }
 
-  const updateArticle = ({ article_id, article }) => {
+  const updateArticle = ( article_id, article ) => {
     // ✨ implement
     // You got this!
+
+    //FETCH AND ADD TO PROPS
+    setMessage('')
+    setSpinnerOn(true)
+    console.log('Article: ', article)
+    console.log('article_id: ', article_id)
+
+    fetch(`${articlesUrl}/${article_id}`, {
+      method: 'PUT',
+      body: JSON.stringify({
+        title: article.title,
+        text: article.text,
+        topic: article.topic
+      }),
+      headers: new Headers({
+        'Content-Type' : 'application/json',
+        Authorization : localStorage.getItem('token')
+      })
+    })
+    .then(res => {
+      if(!res.ok){
+        throw new Error("Can't POST article", res.status)
+      }
+      return res.json()
+    })
+    .then(data => {
+      console.log("PUT articles data: ", data)
+      setMessage(data.message)
+      setSpinnerOn(false)
+      
+      
+    })
+    .catch(error => {
+      if(error?.response?.status === 401) redirectToLogin()
+      setSpinnerOn(false)
+    })
+    
+
   }
 
-  const deleteArticle = article_id => {
+  const deleteArticle = (article_id) => {
     // ✨ implement
+
+    //FETCH AND ADD TO PROPS
+    setMessage('')
+    setSpinnerOn(true)
+    try{
+    fetch(`${articlesUrl}/${article_id}`, {
+      method: 'DELETE',
+      headers: new Headers({
+        'Content-Type' : 'application/json',
+        Authorization : localStorage.getItem('token')
+      })
+    })
+    .then(res => {
+      if(!res.ok){
+        throw new Error("Can't DELETE article", res.status)
+      }
+      return res.json()
+    })
+    .then(data => {
+      console.log("DELETE articles data: ", data)
+      //setMessage(data.message)
+      setArticles(articles)
+      setSpinnerOn(false)
+      setMessage(data.message)
+      
+    })
+    }catch(error) {
+      if(error?.response?.status === 401) {redirectToLogin()
+      setSpinnerOn(false)
+      
+      }
+    }
+    
+
+
   }
 
   return (
     // ✨ fix the JSX: `Spinner`, `Message`, `LoginForm`, `ArticleForm` and `Articles` expect props ❗
     <>
-      <Spinner />
-      <Message />
+      <Spinner 
+        on={spinnerOn}
+        />
+      <Message 
+        message={message}
+        />
       <button id="logout" onClick={logout}>Logout from app</button>
       <div id="wrapper" style={{ opacity: spinnerOn ? "0.25" : "1" }}> {/* <-- do not change this line */}
         <h1>Advanced Web Applications</h1>
@@ -78,11 +249,27 @@ export default function App() {
           <NavLink id="articlesScreen" to="/articles">Articles</NavLink>
         </nav>
         <Routes>
-          <Route path="/" element={<LoginForm />} />
+          <Route path="/" element={<LoginForm login={login}/>} />
           <Route path="articles" element={
             <>
-              <ArticleForm />
-              <Articles />
+              <ArticleForm 
+                currentArticle={currentArticleId && articles.find(a => a.article_id === currentArticleId)}
+                getArticles={getArticles}
+                postArticle={postArticle}
+                updateArticle={updateArticle}
+                deleteArticle={deleteArticle}
+                setCurrentArticleId={setCurrentArticleId}
+                />
+              <Articles 
+                articles={articles}
+                getArticles={getArticles}
+                postArticle={postArticle}
+                updateArticle={updateArticle}
+                deleteArticle={deleteArticle}
+                currentArticleId={currentArticleId}
+                setCurrentArticleId={setCurrentArticleId}
+                redirectToLogin={redirectToLogin}
+                />
             </>
           } />
         </Routes>
--- frontend/components/LoginForm.js
+++ frontend/components/LoginForm.js
@@ -5,9 +5,10 @@ const initialFormValues = {
   username: '',
   password: '',
 }
-export default function LoginForm(props) {
+export default function LoginForm({login}) {
   const [values, setValues] = useState(initialFormValues)
   // ✨ where are my props? Destructure them here
+  
 
   const onChange = evt => {
     const { id, value } = evt.target
@@ -17,6 +18,8 @@ export default function LoginForm(props) {
   const onSubmit = evt => {
     evt.preventDefault()
     // ✨ implement
+    login(values)
+    
   }
 
   const isDisabled = () => {
@@ -24,6 +27,7 @@ export default function LoginForm(props) {
     // Trimmed username must be >= 3, and
     // trimmed password must be >= 8 for
     // the button to become enabled
+    return values.username.trim().length >= 3 && values.password.trim().length >= 8? false : true
   }
 
   return (
